0b442b059df8827d20cb92ca150065eed6ba9145
unknown <thejasjm06@gmail.com>
2024-04-17 14:39:42 +0530

    complete react components

--- react_components/src/components/Greeting.jsx
+++ react_components/src/components/Greeting.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Greeting.css";
+import * as pie from "./math";
+import pi, { pi2, pi3, add, multiply, divide, subtract } from "./math";
+
+console.log(pi);
+
 let Greet;
 var hrs = new Date().getHours();
 
@@ -19,9 +24,18 @@ if (hrs < 12) {
 }
 function Greeting() {
   return (
-    <h1 className="heading" style={greetCol}>
-      {Greet}
-    </h1>
+    <>
+      <h1 className="heading" style={greetCol}>
+        {Greet}
+      </h1>
+      <p>{pie.pi2()}</p>
+      <ul>
+        <li>{pie.add(1, 2)}</li>
+        <li>{multiply(2, 3)}</li>
+        <li>{pie.subtract(7, 2)}</li>
+        <li>{divide(5, 2)}</li>
+      </ul>
+    </>
   );
 }
 
---
+++ react_components/src/components/math.js
@@ -0,0 +1,28 @@
+var pi = 3.14317;
+
+function pi2() {
+  return pi * 2;
+}
+
+function pi3() {
+  return pi * 3;
+}
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+export default pi;
+export { pi2, pi3, add, multiply, divide, subtract };
