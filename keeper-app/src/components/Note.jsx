dc65919889e360b51463b7d445bf289b21df77fe
unknown <thejasjm06@gmail.com>
2024-04-17 17:53:38 +0530

    complete keeper app part 1

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Title of Note</h1>
+      <p>Content for this particular note</p>
+    </div>
+  );
+}
+
+export default Note;
