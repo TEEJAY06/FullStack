71a9c6128ebcf912c941f2228fb607b51bba28e5
unknown <thejasjm06@gmail.com>
2024-04-20 08:24:37 +0530

    complete conditional rendering

--- conditional_rendering/src/components/App.jsx
+++ conditional_rendering/src/components/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import "../App.css";
 import Login from "./Login";
+import Form from "./RegForm";
+
+var isLoggedIn = false;
+var isRegistered = false;
+
+function renderConditionally() {
+  if (isLoggedIn == true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login></Login>;
+  }
+}
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <Login />
+      <Form isRegistered={isRegistered}></Form>
     </div>
   );
 }
